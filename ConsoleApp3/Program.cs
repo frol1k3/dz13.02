153a14e62a4288cfb281102aacd62a192597e671
frol1k3 <133392305+frol1k3@users.noreply.github.com>
2024-03-19 19:26:57 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void print(params int[] numbers)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine($"Номер {i} число {numbers[i]}");
+                }
+            }
+
+            print(45, 7, 9, 5, 6, 33, 67, 8);
+        }
+    }
+}
